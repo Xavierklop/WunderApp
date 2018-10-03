ebf5e7a5baf06130b5fa1cd24ee01e2e733b35f8
Hao Wu <haowu@haos-macbook-pro.local>
2018-10-03 14:22:27 +0200

    Initial Commit

---
+++ WunderApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WunderApp
+//
+//  Created by Hao Wu on 2018/10/3.
+//  Copyright © 2018年 Hao Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
